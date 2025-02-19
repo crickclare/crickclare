5a8bc751edb2900265249d95ff72c8ee1180586c
crickclare <infocrickclare@gmail.com>
2025-02-19 15:51:15 +0530

    seo changes add

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,17 +6,37 @@ import Footer from "@/components/Footer"
 import ScrollToTop from "@/components/ScrollToTop"
 import BackToTop from "@/components/BackToTop"
 import '@fortawesome/fontawesome-free/css/all.css';
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Latest and Breaking Cricket News | Crick Clare",
   description: "Get Schedules of International and Domestic cricket matches along with Latest News and ICC Cricket Rankings of Players on CrickClare",
+  verification: {
+    google: 'l1QUfQY6iO_z2GQnPmRrK3i3U2MKhyAssi4hezQZ3Wk',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JSD7HZC2E3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JSD7HZC2E3');
+          `}
+        </Script>
+        <meta name="google-site-verification" content="l1QUfQY6iO_z2GQnPmRrK3i3U2MKhyAssi4hezQZ3Wk" />
+      </head>
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider>
           <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
